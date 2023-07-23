c4d20a3e94732906b9c84b33eff5127c2216592b
Carlos Marques <chmarques93@gmail.com>
2023-07-23 16:20:39 -0300

    Users entity and resource

--- src/main/java/com/marquesch/course/CourseApplication.java
+++ src/main/java/com/marquesch/course/CourseApplication.java
@@ -1,13 +1,13 @@
-package com.marquesch.demo;
+package com.marquesch.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CourseApplication.class, args);
 	}
 
 }
