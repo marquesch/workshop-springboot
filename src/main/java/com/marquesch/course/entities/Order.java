e24021af4124eea5afbfb049ad096ae9f0a7f5f0
Carlos Marques <chmarques93@gmail.com>
2023-07-29 00:29:27 -0300

    Subtotal & Total methods

--- src/main/java/com/marquesch/course/entities/Order.java
+++ src/main/java/com/marquesch/course/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 		}
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem item : items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
