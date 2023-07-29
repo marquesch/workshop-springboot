aa0392a2d58b16236a7ac075d59054a1feff11d8
Carlos Marques <chmarques93@gmail.com>
2023-07-29 05:24:21 -0300

    User Update

--- src/main/java/com/marquesch/course/services/UserService.java
+++ src/main/java/com/marquesch/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.marquesch.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Service;
 
 import com.marquesch.course.entities.User;
@@ -28,6 +29,18 @@ public class UserService {
 	
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 	
 }
