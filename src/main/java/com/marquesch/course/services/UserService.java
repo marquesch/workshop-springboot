bd09e09e024f2a47e100ff145d267e12cccdd419
Carlos Marques <chmarques93@gmail.com>
2023-07-29 05:50:50 -0300

    Exception handling - delete

--- src/main/java/com/marquesch/course/services/UserService.java
+++ src/main/java/com/marquesch/course/services/UserService.java
@@ -3,10 +3,13 @@ package com.marquesch.course.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.marquesch.course.entities.User;
 import com.marquesch.course.repositories.UserRepository;
+import com.marquesch.course.services.exceptions.DatabaseException;
 import com.marquesch.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +31,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
